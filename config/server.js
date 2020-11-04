b8b59e0b1e8f4ea0beadb631040582065b9e9b71
Bruno Brian da Silva Sousa <bruno.brian@BRSPOAMEANM0005.local>
2020-11-04 12:33:44 -0300

    Initial Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2ae54f967660de5b0a3da510ec43d608'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '3d3c4a57-db5b-4579-b38c-ae7cd9c7122e'
+};
