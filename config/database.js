e04a877ce0b69db51c2762391663af6babf96b64
Bruno Brian da Silva Sousa <bruninho_brian@hotmail.com>
2021-04-22 21:11:46 -0300

    fix

--- config/database.js
+++ config/database.js
@@ -10,10 +10,9 @@ module.exports = ({ env }) => ({
         database: env('DATABASE_NAME', 'blog-strapi'),
         username: env('DATABASE_USERNAME', 'postgres'),
         password: env('DATABASE_PASSWORD', 'postgres123456'),
+        ssl: env.bool('DATABASE_SSL', false),
       },
-      options: {
-        ssl:true,
-      },
+      options: {}
     },
   },
 });
